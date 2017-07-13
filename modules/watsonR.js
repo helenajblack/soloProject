74cdbcd9a7ecff479db5d69ba12207461cbc64e9
Helena Black <helenajblack@yahoo.com>
2017-07-13 15:20:48 -0500

    created wavfile

--- modules/watsonR.js
+++ modules/watsonR.js
@@ -4,30 +4,41 @@ var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true
 }));
 
+//object defined
 var text_to_speech = new TextToSpeechV1({
   username: watsonCredentials.username,
   password: watsonCredentials.password
 });
 
-var params = {
-  voice: 'en-US_AllisonVoice'
-};
-
-text_to_speech.voice(params, function(error, voice) {
-  if (error)
-    console.log('Error:', error);
-  else
-    console.log(JSON.stringify(voice, null, 2));
-});
-
+//post request
 router.post('/', function(req, res) {
   console.log(req.body.text);
+
+  var params = {
+    text: req.body.text,
+    voice: 'en-US_AllisonVoice',
+    accept: 'audio/wav'
+  };
+
+  text_to_speech.voice(params, function(error, voice) {
+    if (error)
+      console.log('Error:', error);
+    else
+      console.log(JSON.stringify(voice, null, 2));
+  });
+
+  // Pipe the synthesized text to a file.
+  text_to_speech.synthesize(params).on('error', function(error) {
+    console.log('Error:', error);
+  }).pipe(fs.createWriteStream('public/soundfiles/watson.wav'));
+
   res.send('something');
 });
 
